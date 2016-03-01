995b2020d95efc5a34d242896e5f8d74657419e4
nanthony21 <nickmanthony@hotmail.com>
2016-02-29 18:44:57 -0600

    fixed drawing errors

--- Python/nickosa4.py
+++ Python/nickosa4.py
@@ -10,17 +10,13 @@ q3=y
 q4=z
 
 """
-magplot=True
 magcalibrate=False
 magcalcheck=False
 
-import serial
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-from matplotlib.pylab import ion
-from matplotlib.patches import FancyArrowPatch
-from mpl_toolkits.mplot3d import proj3d
+#from mpl_toolkits.mplot3d import proj3d
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import scipy.linalg as la
@@ -50,25 +46,6 @@ win.window.wm_geometry("+0+0")
 axis.view_init(25,90)
 plt.show(block=False)
 
-#initiate optional plotting objects.
-if magplot:
-    fig2=plt.figure()
-    win2=get_current_fig_manager()
-    win2.window.wm_geometry("+0+600")
-    axis3=fig2.add_subplot(121)
-    axis4=fig2.add_subplot(122)
-    axis3.set_aspect('equal')
-    axis3.plot([0,0],[0,0])
-    axis3.set_xlim([-1,1])
-    axis3.set_ylim([-1,1])
-    axis4.set_aspect('equal')
-    axis4.plot([0,0],[0,0])
-    axis4.set_xlim([-1,1])
-    axis4.set_ylim([-1,1])
-    plt.show(block=False)
-
-
-
 
 
 
@@ -92,17 +69,20 @@ iovec4=np.array([1,0,0])
 
 drone=Drone('COM14')
 drone.setStream(True)
+
 #Loop variables.
 cond=True
 v=0
 yaw2=0
 
-while cond:
-    try:
+try:
+    while cond:
+
         #Serial data transfer        
         q,m,a,rm,skippedBytes=drone.dataQueue.get()
-        #print(skippedBytes)
-        print(q)
+       
+        print(skippedBytes)
+
         
         #print('r','%.1f' %np.rad2deg(np.arctan2(2*(q[0]*q[1]+q[2]*q[3]),1-2*(q[1]**2+q[2]**2))),'y','%.1f'%np.rad2deg(np.arctan2(2*(q[0]*q[3]+q[1]*q[2]),1-2*(q[2]**2+q[3]**2))),'p', '%.1f'%np.rad2deg(np.arcsin(2*(q[0]*q[2]-q[3]*q[1]))))   
           
@@ -122,6 +102,22 @@ while cond:
         ovec4=rotate2world(q,iovec4)
  
         #Plotting
+        try:
+            accelline[0].remove()
+            accel.remove()
+            mag.remove()       
+            project.remove()
+            magline[0].remove()
+            projline[0].remove()
+            gpline[0].remove()
+            mpline[0].remove()
+            qline[0].remove()
+            qline2[0].remove()
+            qline3[0].remove()
+            qline4[0].remove()
+        except:
+            pass
+        
         mag=axis.scatter(m[0],m[1],m[2],color='g')
         project=axis.scatter(proj[0],proj[1],proj[2],color='c')   
         accelline=axis.plot([0,a[0]], [0,a[1]],[0,a[2]],color='m')
@@ -136,73 +132,51 @@ while cond:
         qline4=axis.plot([0,ovec4[0]],[0,ovec4[1]],[0,ovec4[2]],color='r')        
         
         fig.canvas.draw()
-        
-        accelline[0].remove()
-        accel.remove()
-        mag.remove()       
-        project.remove()
-        magline[0].remove()
-        projline[0].remove()
-        gpline[0].remove()
-        mpline[0].remove()
-        qline[0].remove()
-        qline2[0].remove()
-        qline3[0].remove()
-        qline4[0].remove()
-        
-        #Optional plotting
-        if magplot:
-            mspecial=axis3.plot([0,a[0]],[0,a[2]],color='b')
-            mspecial3=axis4.plot([0,a[1]],[0,a[2]],color='g')            
-            fig2.canvas.draw()
-            mspecial[0].remove()
-            mspecial3[0].remove()
+
         
         #Magnetic data accumulation for calibration.
         if magcalibrate or magcalcheck:
             magarr=np.vstack([magarr,rm])
         
 
-    except KeyboardInterrupt:
-        cond=False
-        time.sleep(0.1)
+finally:
+    cond=False
+    time.sleep(0.1)
 
-        drone.exit=True
-        plt.close(fig)
-        if magplot:
-            plt.close(fig2)
+    drone.exit=True
+    plt.close(fig)
+    
+    if magcalibrate or magcalcheck:
+        #run a magnetometer calibration 
+        magarr=np.delete(magarr,0,axis=0)
+        calmagarr=np.zeros(magarr.shape)
+        print("Fitting Ellipse")
+        ell=fitellipse(magarr)
+        offsets=ell[1]
+        invw=la.sqrtm(ell[0])
         
-        if magcalibrate or magcalcheck:
-            #run a magnetometer calibration 
-            magarr=np.delete(magarr,0,axis=0)
-            calmagarr=np.zeros(magarr.shape)
-            print("Fitting Ellipse")
-            ell=fitellipse(magarr)
-            offsets=ell[1]
-            invw=la.sqrtm(ell[0])
-            
-            for i in range(magarr.shape[0]):
-                calmagarr[i,0],calmagarr[i,1],calmagarr[i,2]=calibratemag(magarr[i],offsets,invw)
-            
-            
-            print("Plotting")
-            fig=plt.figure()
-            axis2=fig.gca(projection="3d")
-            axis2.scatter(0,0,0,color='r')
-            for i in range(magarr.shape[0]):
-                axis2.scatter(magarr[i,0],magarr[i,1],magarr[i,2])
-                axis2.scatter(calmagarr[i,0],calmagarr[i,1],calmagarr[i,2],color='g')
-            axis2.set_xlabel('x')
-            axis2.set_ylabel('y')
-            axis2.set_zlabel('z')
-            axis2.set_aspect("equal")
-            axis2.set_xlim([-ra,ra])
-            axis2.set_ylim([-ra,ra])
-            axis2.set_zlim([-ra,ra])
-            print(invw)
-            print(offsets)
-            
-            
-            
-            
-            plt.show()
+        for i in range(magarr.shape[0]):
+            calmagarr[i,0],calmagarr[i,1],calmagarr[i,2]=calibratemag(magarr[i],offsets,invw)
+        
+        
+        print("Plotting")
+        fig=plt.figure()
+        axis2=fig.gca(projection="3d")
+        axis2.scatter(0,0,0,color='r')
+        for i in range(magarr.shape[0]):
+            axis2.scatter(magarr[i,0],magarr[i,1],magarr[i,2])
+            axis2.scatter(calmagarr[i,0],calmagarr[i,1],calmagarr[i,2],color='g')
+        axis2.set_xlabel('x')
+        axis2.set_ylabel('y')
+        axis2.set_zlabel('z')
+        axis2.set_aspect("equal")
+        axis2.set_xlim([-ra,ra])
+        axis2.set_ylim([-ra,ra])
+        axis2.set_zlim([-ra,ra])
+        print(invw)
+        print(offsets)
+        
+        
+        
+        
+        plt.show()
