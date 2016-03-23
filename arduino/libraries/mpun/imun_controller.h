0e31d0af39cc96a3ae35be6aebd137aae09588b5
nanthony21 <nickmanthony@hotmail.com>
2016-03-22 21:29:20 -0500

    Deleted PID class. will use the arduino class instead

--- arduino/libraries/mpun/imun_controller.h
+++ arduino/libraries/mpun/imun_controller.h
@@ -5,56 +5,6 @@
 #include <Arduino.h>
 
 
-class PID
-{
-	float output=0,P,I,D,integral=0,lastValue,outMax,outMin;
-	int lastTimeStamp;
-	public:
-		PID(float p, float i, float d, float minOut, float maxOut); 
-		float calculate(float currentValue,float setValue,int timeStamp);
-		void setPID(float p,float i,float d);
-	private:
-		void setOutputLimits(float Min, float Max);
-		
-};
-
-PID::PID(float p, float i, float d, float minOut, float maxOut){
-	setPID(p,i,d)
-	setOutputLimits(minOut,maxOut);
-}
-		
-//Does time need to be in seconds?
-float PID::calculate(float currentValue,float setValue,int timeStamp){
-	float error=currentValue-setValue;
-	integral+=error*(timeStamp-lastTimeStamp);
-	if (integral>outMax) integral=outMax;
-	else if (integral<outMin) integral=outMin;
-	float derivative=(currentValue-lastValue)/(timeStamp-lastTimeStamp);
-	lastValue=currentValue;
-	lastTimeStamp=timestamp;
-	output=P*error+I*integral+D*derivative;
-	if (output>outMax) output=outMax;
-	else if (output<outMin) output=outMin;
-}
-
-void PID::setOutputLimits(float Min, float Max)
-{
-   if(Min > Max) return;
-   outMin = Min;
-   outMax = Max;
-    
-   if(output > outMax) output = outMax;
-   else if(output < outMin) output = outMin;
- 
-   if(integral> outMax) integral= outMax;
-   else if(integral< outMin) integral= outMin;
-}
-
-void PID::setPID(float p, float i, float d){
-	P=p;
-	I=i;
-	D=d;
-}
 
 class PIDMaster{
 	PIDMaster(ImunQuadMotor &qm,Quaternion &q);
